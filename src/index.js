8fcaddb6d2353a08168bf8aa6ad4d7d221d1da58
Nick <65170141+kubarenatioN@users.noreply.github.com>
2021-01-21 15:33:10 +0300

    feat: solve min, max and avg array problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,35 @@
 
-exports.min = function min (array) {
-  return 0;
-}
+exports.min = min
+exports.max = max
+exports.avg = avg
 
-exports.max = function max (array) {
-  return 0;
+function min(arr){
+  if (arr == undefined || arr.length == 0) {
+    return 0
+  }
+  let min = arr[0]
+  arr.forEach(el => {
+    if (el < min) min = el
+  })
+  return min
 }
-
-exports.avg = function avg (array) {
-  return 0;
+function max(arr){
+  if (arr == undefined || arr.length == 0) {
+    return 0
+  }
+  let max = arr[0]
+  arr.forEach(el => {
+    if (el > max) max = el
+  })
+  return max
+}
+function avg(arr){
+  if (arr == undefined || arr.length == 0) {
+    return 0
+  }
+  let avg = 0
+  arr.forEach(el => {
+    avg += el
+  })
+  return avg/arr.length
 }
